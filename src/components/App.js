476a4cf245ca3996c1463bd4ba56e0dba8b5269d
Stephanie Riera <stephanie@frontside.io>
2016-12-28 20:25:49 -0600

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Header from './Header';
+import Inventory from './Inventory';
+import Order from './Order';
+
+
+
+class App extends React.Component {
+constructor() {
+	super();
+
+	this.addFish = this.addFish.bind(this);
+	// getInitialState
+	this.state = {
+		fishes: {},
+		order: {}
+	};
+}
+
+addFish(fish) {
+	const fishes = {...this.state.fishes};
+	const timestamp = Date.now();
+	fishes[`fish-${timestamp}`] = fish;
+	this.setState({ fishes: fishes });
+}
+
+render() {
+	return (
+		<div className="catch-of-the-day">
+			<div className="menu">
+				<Header tagline="Fresh Seafood Market"/>
+			</div>
+			<Order />
+			<Inventory addFish={this.addFish} />
+		</div>
+	)
+}
+
+export default App;
